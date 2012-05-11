81a145f86d86ba04e5684693751df1fcf1188065
Justin Chudgar <justin@justinzane.com>
2012-05-10 22:50:50 -0700

    Added custom theming. Still fugly, but nicely darker.

--- src/django_bodystats/BodyStats/static/app/view/Header.js
+++ src/django_bodystats/BodyStats/static/app/view/Header.js
@@ -2,10 +2,10 @@ Ext.define('BodyStats.view.Header', {
 	extend: 'Ext.panel.Panel',
 	alias: 'widget.headerpanel',
 	bodyStyle: {
-		background: '#00007f',
+		//background: '#00007f',
 		padding: '10px',
 		height: '48px',
 	},
-	html: '<img src="static/resources/images/bodystats_logo_white.svg" alt="logo" />'
+	//html: '<img src="static/resources/images/bodystats_logo_white.svg" alt="logo" />'
 });
 
